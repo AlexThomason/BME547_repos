5f8a78c7533cae315734a4e378dd7913a6e4b3b1
Alex Thomason <AlexThomason@Alexs-MacBook-Pro-7.local>
2021-08-31 08:50:37 -0400

    Added LDL functions and option to run LDL analysis

--- interface.py
+++ interface.py
@@ -10,6 +10,7 @@ def interface():
         print("Options:")
         print("0 = Quit")
         print("1 = HDL Analysis")
+        print("2 = LDL Analysis")
         choice = int(input("Make a choice: "))
         print(type(choice))
         
@@ -17,9 +18,15 @@ def interface():
             keep_running = False
         elif choice == 1:
             HDL_Driver()
+        elif choice == 2:
+            LDL_Driver()
+        else :
+            print ("Not an option - Please try again")    
 
     return choice
    
+
+# HDL Functions
 
 def HDL_input():
     HDL_value = int(input(("Enter HDL Value: ")))
@@ -43,5 +50,30 @@ def HDL_output(value, character) :
     print("This HDL level is {}".format(character))
 
 
+
+# LDL Functions
+
+def LDL_input():
+    LDL_value = int(input(("Enter LDL Value: ")))
+    return LDL_value
+
+def HDL_Driver():
+    LDL_value = LDL_input()
+    LDL_character = check_HDL(LDL_value)
+    HDL_output(LDL_value,LDL_character)
+
+def check_HDL(value):
+    if value < 130:
+        return "Normal"
+    elif 130 <= value <= 159:
+        return "Borderline High"
+    elif 160 <= value <= 189:
+        return "High"
+    else:
+        return "High"
+
+def HDL_output(value, character) :
+    print("Your HDL level is {}".format(value))
+    print("This HDL level is {}".format(character))
 
 interface()
