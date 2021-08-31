464740d5179aa2a2f5e5688d887cd15f0f0fca6b
Alex Thomason <AlexThomason@Alexs-MacBook-Pro-7.local>
2021-08-30 22:44:36 -0400

    initial commit of interface.py - Addition of interface() function

---
+++ interface.py
@@ -0,0 +1,13 @@
+# returns.py
+# Author Alex Thomason
+
+
+def interface():
+    print("My Program")
+    print("Options:")
+    print("9 - Quit")
+    choice = input("Enter your choice: ")
+    if choice=='9':
+        return
+   
+interface()
